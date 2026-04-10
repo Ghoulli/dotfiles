6d4b82f8f035f92b733864b0da78cade62b897ec
Ghoulli <quintnoah@gmail.com>
2026-04-10 23:21:12 +0200

    gruvbox dark alacritty + fish + starship

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,11 @@
+# Starship prompt
+starship init fish | source
+
+# Useful aliases
+alias ls='ls --color=auto'
+alias ll='ls -lah --color=auto'
+alias grep='grep --color=auto'
+alias dotfiles='cd ~/dotfiles'
+
+# Clean greeting
+set fish_greeting ""
